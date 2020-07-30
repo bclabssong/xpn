554494b94ada2adbba17b808507019acbc0ce484
bclabssong <song@pantheonx.io>
2020-07-30 18:20:28 +0900

    style: log change korean to english in Whitelist.js

--- test/Whitelist.js
+++ test/Whitelist.js
@@ -1,18 +1,18 @@
 const Whitelist = artifacts.require("Whitelist");
 
 contract("Whitelist", (accounts) => {
-  it("화이트리스트 컨트랙트 배포 여부 확인", async () => {
+  it("Whitelist contract deployment check", async () => {
     const whitelist = await Whitelist.deployed();
     const balance = await whitelist.balanceOf(accounts[0]);
-    assert.equal(balance.valueOf(), 0, "컨트랙트가 정상 동작 하지 않습니다.");
+    assert.equal(balance.valueOf(), 0, "Contract is not deployed");
   });
 
-  it("화이트리스 정상 등록 되는지 확인", async () => {
-    const metadata = "아무거나";
+  it("Whitelist enrollWhiteList function check", async () => {
+    const metadata = "blabla";
     const whitelist = await Whitelist.deployed();
 
     await whitelist.enrollWhiteList(accounts[0], metadata, {from: accounts[0]});
     const balance = await whitelist.balanceOf(accounts[0]);
-    assert.equal(balance.valueOf(), 1, "화이트리스트가 정상 등록되지 않았습니다.");
+    assert.equal(balance.valueOf(), 1, "Whitelist is not enrolled");
   });
 })
