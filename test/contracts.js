9d524d442c4b89716a649ee738546d912c2d340f
bclabssong <song@pantheonx.io>
2020-07-31 11:11:52 +0900

    test: test code address fix

--- test/contracts.js
+++ test/contracts.js
@@ -13,6 +13,8 @@ contract("YPN, Whitelist, Swap", (accounts) => {
   });
 
   it("YPN token send to Swap CA", async () => {
+    const decimals = 18;
+    const transferSupply = 770000000 * 10 ** decimals;
     const ypnToken = await YPN.deployed();
     const swapContract = await Swap.deployed();
     const swapAddress = swapContract.address;
@@ -20,13 +22,12 @@ contract("YPN, Whitelist, Swap", (accounts) => {
     await ypnToken.transfer(swapAddress, "770000000000000000000000000", {from: accounts[0]});
 
     const swapBalance = await ypnToken.balanceOf(swapAddress);
-    console.log(swapBalance);
-    assert.equal(swapBalance.valueOf(), 770000000000000000000000000, "Token send is wrong");
+    assert.equal(swapBalance.valueOf(), transferSupply, "Token send is wrong");
   });
 
   it("Whitelist contract deployment check", async () => {
     const whitelist = await Whitelist.deployed();
-    const balance = await whitelist.balanceOf(accounts[0]);
+    const balance = await whitelist.balanceOf(accounts[1]);
     assert.equal(balance.valueOf(), 0, "Whitelist contract is not deployed");
   });
 
@@ -34,8 +35,8 @@ contract("YPN, Whitelist, Swap", (accounts) => {
     const metadata = "blabla";
     const whitelist = await Whitelist.deployed();
 
-    await whitelist.enrollWhiteList(accounts[0], metadata, {from: accounts[0]});
-    const balance = await whitelist.balanceOf(accounts[0]);
+    await whitelist.enrollWhiteList(accounts[1], metadata, {from: accounts[0]});
+    const balance = await whitelist.balanceOf(accounts[1]);
     assert.equal(balance.valueOf(), 1, "Whitelist is not enrolled");
   });
 
@@ -43,7 +44,8 @@ contract("YPN, Whitelist, Swap", (accounts) => {
     const tokenMount = "770000000000000000000000000";
     const SwapContract = await Swap.deployed();
     const SwapCA = SwapContract.address;
-    const newTokenBalance = await SwapContract.newTokenContractBalance({from: SwapCA});
+
+    const newTokenBalance = await SwapContract.newTokenContractBalance();
     assert.equal(newTokenBalance.valueOf(), tokenMount, "Swap contract is not deployed");
   });
 });
