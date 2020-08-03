4e4ff5850bf15caabefd009236ea55c7147877f4
bclabssong <song@pantheonx.io>
2020-08-03 12:00:24 +0900

    test: add ExToken test code

--- test/contracts.js
+++ test/contracts.js
@@ -1,9 +1,31 @@
 const YPN = artifacts.require("YPN");
 const Whitelist = artifacts.require("Whitelist");
 const Swap = artifacts.require("Swap");
+const EXT = artifacts.require("ExToken");
 
-contract("YPN, Whitelist, Swap", (accounts) => {
-  it("YPN, Whitelist, Swap contracts deployment check", async () => {
+contract("YPN, Whitelist, Swap, EXT test", (accounts) => {
+  it("EXT contract deployment check", async () => {
+    const decimals = 18;
+    const totalSupply = 770000000 * 10 ** decimals;
+    const exToken = await EXT.deployed();
+
+    const deployerBalance = await exToken.balanceOf(accounts[0]);
+    assert.equal(deployerBalance.valueOf(), totalSupply, "Contract is not deployed");
+  });
+
+  it("ExToken approve to Swap CA", async () => {
+    const tokenMount = "770000000000000000000000000";
+    const exToken = await EXT.deployed();
+    const SwapContract = await Swap.deployed();
+    const SwapCA = SwapContract.address;
+
+    exToken.transfer(accounts[1], tokenMount, {from: accounts[0]});
+    exToken.approve(SwapCA ,tokenMount, {from: accounts[1]});
+    const ac1Balance = await exToken.balanceOf(accounts[1]);
+    assert.equal(ac1Balance.valueOf(), tokenMount, "transfer is wrong");
+  });
+
+  it("YPN contract deployment check", async () => {
     const decimals = 18;
     const totalSupply = 770000000 * 10 ** decimals;
     const ypnToken = await YPN.deployed();
@@ -48,4 +70,14 @@ contract("YPN, Whitelist, Swap", (accounts) => {
     const newTokenBalance = await SwapContract.newTokenContractBalance();
     assert.equal(newTokenBalance.valueOf(), tokenMount, "Swap contract is not deployed");
   });
+
+  // it("Token Swap check", async () => {
+  //   const tokenMount = "770000000000000000000000000";
+  //   const SwapContract = await Swap.deployed();
+  //   const SwapCA = SwapContract.address;
+
+  //   await SwapContract.tokenSwap(tokenMount, {from: accounts[1]});
+  //   const newTokenBalance = await SwapContract.newTokenContractBalance();
+  //   assert.equal(newTokenBalance.valueOf(), 0, "tokenSwap function is wrong");
+  // });
 });
